89d7202ec5a3bb406300a777231b13f2ef39a639
Tareq Hasan <tareq1988@gmail.com>
2013-11-27 21:12:04 +0600

    post content and titles added

--- templates/reports.php
+++ templates/reports.php
@@ -10,9 +10,27 @@ get_header();
 
     <?php dokan_get_template( __DIR__ . '/dashboard-nav.php', array( 'active_menu' => 'reports' ) ); ?>
 
-    <div class="col-md-9">
-        Reports
-    </div>
+    <div id="primary" class="content-area col-md-9">
+        <div id="content" class="site-content" role="main">
+
+            <?php while (have_posts()) : the_post(); ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div><!-- .entry-content -->
+
+                </article>
+
+            <?php endwhile; // end of the loop. ?>
+
+        </div><!-- #content .site-content -->
+    </div><!-- #primary .content-area -->
+</div> <!-- .row -->
 </div>
 
 <?php get_footer(); ?>
