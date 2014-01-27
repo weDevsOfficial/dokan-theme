bd15dbcad3decb83d37fae0e28a6ad4ccb05f59c
Tareq Hasan <tareq1988@gmail.com>
2014-01-28 03:14:51 +0600

    pending and approved subsubsub added on withdraw admin page

--- classes/template-withdraw.php
+++ classes/template-withdraw.php
@@ -169,17 +169,8 @@ class Dokan_Template_Withdraw {
         );
 
         $update = $this->insert_withdraw( $data_info );
-        if ( !defined('DOING_AJAX') && DOING_AJAX !== true &&  $update) {
-            wp_redirect( add_query_arg( array( 'message' => 'request_success' ), get_permalink() ) );
-        }
 
-        if ( defined('DOING_AJAX') && DOING_AJAX === true &&  $update) {
-
-            return $update;
-
-        }
-
-
+        wp_redirect( add_query_arg( array( 'message' => 'request_success' ), get_permalink() ) );
     }
 
     function has_pending_request( $user_id ) {
@@ -188,10 +179,10 @@ class Dokan_Template_Withdraw {
         $wpdb->dokan_withdraw = $wpdb->prefix . 'dokan_withdraw';
 
         $status = $wpdb->get_results( $wpdb->prepare(
-                        "SELECT id
+            "SELECT id
              FROM $wpdb->dokan_withdraw
              WHERE user_id = %d AND status = 0", $user_id
-                ) );
+        ) );
 
         if ( $status ) {
             return true;
@@ -200,7 +191,7 @@ class Dokan_Template_Withdraw {
         return false;
     }
 
-    function get_withdraw_requests( $user_id='', $status = 0 ) {
+    function get_withdraw_requests( $user_id = '', $status = 0 ) {
         global $wpdb;
 
         $where = empty( $user_id ) ? '' : sprintf( "user_id ='%d' &&", $user_id );
@@ -213,82 +204,11 @@ class Dokan_Template_Withdraw {
         return $result;
     }
 
-    function forntend_admin_withdraw_list() {
-        $user_id = get_current_user_id();
-        $result = $this->get_withdraw_requests($user_id, $status = 0);
-
-        if ( ! count( $result ) ) {
-            echo '<h3>' . __( 'No pending withdraw request found', 'dokan' ) . '</h3>';
-            return;
-        }
+    function admin_withdraw_list( $status ) {
+        $result = $this->get_withdraw_requests( '', $status );
         ?>
         <form method="post" action="">
-            <table class="widefat" style="margin-top: 20px;">
-                <thead>
-                <tr>
-                    <th><?php _e( 'User Name', 'dokan' ); ?></th>
-                    <th><?php _e( 'User Email', 'dokan' ); ?></th>
-                    <th><?php _e( 'Amount', 'dokan' ); ?></th>
-                    <th><?php _e( 'Date', 'dokan' ); ?></th>
-                    <th><?php _e( 'Status', 'dokan' ); ?></th>
-                    <th><?php _e( 'Method', 'dokan' ); ?></th>
-                    <th><?php _e( 'Note', 'dokan' ); ?></th>
-                    <th><?php _e( 'IP', 'dokan' ); ?></th>
-                </tr>
-                </thead>
-
-        <?php
-        foreach( $result as $key=>$result_array ) {
-            $user_data = get_userdata($result_array->user_id);
-            ?>
-
-            <tr>
-                <td><?php echo $user_data->user_login; ?></td>
-                <td><?php echo $user_data->user_email; ?></td>
-                <td><?php echo $result_array->amount; ?></td>
-                <td><?php echo $result_array->date; ?></td>
-                <td>
-                    <?php
-                        echo $this->request_status( $result_array->status );
-                    ?>
-                </td>
-                <td><?php echo $result_array->method; ?></td>
-                <td >
-                    <div class="dokan-add-note" style="width: 130px;">
-                        <p class="ajax_note"><?php echo $result_array->note; ?></p>
-                        <input type="text" class="dokan-note-text" style="display: none;" name="note">
-                        <a class="dokan-note-submit btn btn-info" style="display: none;" data-admin_url="<?php echo admin_url( 'admin-ajax.php' ); ?>" data-row_id=<?php echo $result_array->id; ?> data-user_id=<?php echo $result_array->user_id; ?> href="#" ><?php _e('Note', 'dokan' ); ?></a>
-                        <a href="#" style="display: none; margin-left: 72px;" class="dokan-note-cancle button"><?php _e('X', 'dokan' ); ?></a>
-                        <a href="#" class="dokan-note-field"><?php _e('Add note', 'dokan' ); ?></a>
-                    </div>
-
-                </td>
-                <td><?php echo $result_array->ip; ?></td>
-            </tr>
-            <?php
-
-        }
-        echo '</table>';
-        ?>
-
-        </form>
-
-        <?php
-        $this->add_note_script();
-    }
-
-
-    function admin_withdraw_list() {
-        $user_id = get_current_user_id();
-        $result = $this->get_withdraw_requests($user_id, $status = 0);
-
-        if ( ! count( $result ) ) {
-            echo '<h3>' . __( 'No pending withdraw request found', 'dokan' ) . '</h3>';
-            return;
-        }
-        ?>
-        <form method="post" action="">
-            <table class="widefat" style="margin-top: 20px;">
+            <table class="widefat" style="margin-top: 10px;">
                 <thead>
                 <tr>
                     <th class="check-column"><input type="checkbox" id="cb-select-all-1" class="dokan-withdraw-allcheck"></th>
@@ -303,41 +223,59 @@ class Dokan_Template_Withdraw {
                 </tr>
                 </thead>
 
-        <?php
-        foreach( $result as $key=>$result_array ) {
-            $user_data = get_userdata($result_array->user_id);
+            <?php
+            if ( $result ) {
+                foreach( $result as $key=>$result_array ) {
+                    $user_data = get_userdata($result_array->user_id);
+                    ?>
+
+                    <tr>
+                        <th class="check-column"><input type="checkbox" name="id[]" value="<?php echo $result_array->id;?>"></th>
+                        <th><?php echo $user_data->user_login; ?></th>
+                        <th><?php echo $user_data->user_email; ?></th>
+                        <th><?php echo wc_price( $result_array->amount ); ?></th>
+                        <th><?php echo $result_array->date; ?></th>
+                        <th>
+                            <?php
+                                echo $this->request_status( $result_array->status );
+                            ?>
+                        </th>
+                        <th><?php echo $result_array->method; ?></th>
+                        <th >
+                            <div class="dokan-add-note" style="width: 130px;">
+                                <p class="ajax_note"><?php echo $result_array->note; ?></p>
+                                <input type="text" class="dokan-note-text" style="display: none;" name="note">
+                                <a class="dokan-note-submit btn btn-info" style="display: none;" data-admin_url="<?php echo admin_url( 'admin-ajax.php' ); ?>" data-row_id=<?php echo $result_array->id; ?> data-user_id=<?php echo $result_array->user_id; ?> href="#" ><?php _e('Note', 'dokan' ); ?></a>
+                                <a href="#" style="display: none; margin-left: 72px;" class="dokan-note-cancle button"><?php _e('X', 'dokan' ); ?></a>
+                                <a href="#" class="dokan-note-field"><?php _e('Add note', 'dokan' ); ?></a>
+                            </div>
+
+                        </th>
+                        <th><?php echo $result_array->ip; ?></th>
+                    </tr>
+                    <?php
+
+                }
+            } else {
+                ?>
+
+                <tr>
+                    <td colspan="8">
+                        <?php _e( 'No result found', 'dokan' ) ?>
+                    </td>
+                </tr>
+
+                <?php
+            }
+            ?>
+            </table>
+
+            <?php
+                if ( $result && $status == 0 ) {
+                    submit_button( __( 'Download PayPal mass payment file (csv)', 'dokan' ), 'primary', 'dokan-withdraw-csv' );
+                }
             ?>
 
-            <tr>
-                <th class="check-column"><input type="checkbox" name="id[]" value="<?php echo $result_array->id;?>"></th>
-                <th><?php echo $user_data->user_login; ?></th>
-                <th><?php echo $user_data->user_email; ?></th>
-                <th><?php echo $result_array->amount; ?></th>
-                <th><?php echo $result_array->date; ?></th>
-                <th>
-                    <?php
-                        echo $this->request_status( $result_array->status );
-                    ?>
-                </th>
-                <th><?php echo $result_array->method; ?></th>
-                <th >
-                    <div class="dokan-add-note" style="width: 130px;">
-                        <p class="ajax_note"><?php echo $result_array->note; ?></p>
-                        <input type="text" class="dokan-note-text" style="display: none;" name="note">
-                        <a class="dokan-note-submit btn btn-info" style="display: none;" data-admin_url="<?php echo admin_url( 'admin-ajax.php' ); ?>" data-row_id=<?php echo $result_array->id; ?> data-user_id=<?php echo $result_array->user_id; ?> href="#" ><?php _e('Note', 'dokan' ); ?></a>
-                        <a href="#" style="display: none; margin-left: 72px;" class="dokan-note-cancle button"><?php _e('X', 'dokan' ); ?></a>
-                        <a href="#" class="dokan-note-field"><?php _e('Add note', 'dokan' ); ?></a>
-                    </div>
-
-                </th>
-                <th><?php echo $result_array->ip; ?></th>
-            </tr>
-            <?php
-
-        }
-        echo '</table>';
-        ?>
-        <input type="submit" name="dokan-withdraw-csv" class="button" value="Download">
         </form>
         <?php
 
@@ -438,7 +376,7 @@ class Dokan_Template_Withdraw {
     }
 
     function get_withdraw_limit() {
-        return 50;
+        return dokan_get_option( 'withdraw_limit', 'dokan_selling', '50' );
     }
 
     function get_user_balance( $user_id ) {
@@ -474,7 +412,7 @@ class Dokan_Template_Withdraw {
                 <?php foreach ($withdraw_requests as $request) { ?>
 
                     <tr>
-                        <td><?php echo woocommerce_price( $request->amount ); ?></td>
+                        <td><?php echo wc_price( $request->amount ); ?></td>
                         <td><?php echo $request->method; ?></td>
                         <td><?php echo dokan_format_time( $request->date ); ?></td>
                         <td>
@@ -482,7 +420,7 @@ class Dokan_Template_Withdraw {
                             $url = add_query_arg( array(
                                 'action' => 'dokan_cancel_withdrow',
                                 'id' => $request->id
-                                    ), get_permalink() );
+                            ), get_permalink() );
                             ?>
                             <a href="<?php echo wp_nonce_url( $url, 'dokan_cancel_withdrow' ); ?>">
                                 <?php _e( 'Cancel', 'dokan' ); ?>
@@ -582,10 +520,6 @@ class Dokan_Template_Withdraw {
             <strong class="jquery_error_place"></strong>
         </div>
 
-        <!-- <div class="alert  alert-success" style="display: none;">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong></strong>
-        </div> -->
         <span class="ajax_table_shown"></span>
         <form class="form-horizontal withdraw" role="form" method="post">
             <div class="form-group">
