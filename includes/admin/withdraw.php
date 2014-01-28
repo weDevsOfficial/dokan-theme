cc7d8ae855d181d0eee9d11458eb907d82cd72e3
Tareq Hasan <tareq1988@gmail.com>
2014-01-28 18:03:46 +0600

    counts added on withdraw listing in admin

--- includes/admin/withdraw.php
+++ includes/admin/withdraw.php
@@ -1,17 +1,32 @@
 <?php
-$current = isset( $_GET['status'] ) ? $_GET['status'] : 'pending';
-$status = ( $current == 'pending' ) ? 0 : 1;
+$dokan_admin_withdraw = Dokan_Template_Withdraw::init();
+$counts = $dokan_admin_withdraw->get_withdraw_count();
+
+$status = isset( $_GET['status'] ) ? $_GET['status'] : 'pending';
 ?>
 <div class="wrap">
     <h2><?php _e( 'Withdraw Requests', 'dokan' ); ?></h2>
 
     <ul class="subsubsub" style="float: none;">
-        <li><a href="admin.php?page=dokan-withdraw&amp;status=pending" <?php if ( $current == 'pending' ) echo 'class="current"'; ?>><?php _e( 'Pending', 'dokan' ); ?></a> |</li>
-        <li><a href="admin.php?page=dokan-withdraw&amp;status=completed" <?php if ( $current == 'completed' ) echo 'class="current"'; ?>><?php _e( 'Approved', 'dokan' ); ?></a></li>
+        <li>
+            <a href="admin.php?page=dokan-withdraw&amp;status=pending" <?php if ( $status == 'pending' ) echo 'class="current"'; ?>>
+                <?php _e( 'Pending', 'dokan' ); ?> <span class="count">(<?php echo $counts['pending'] ?>)</span>
+            </a> |
+        </li>
+        <li>
+            <a href="admin.php?page=dokan-withdraw&amp;status=completed" <?php if ( $status == 'completed' ) echo 'class="current"'; ?>>
+                <?php _e( 'Approved', 'dokan' ); ?> <span class="count">(<?php echo $counts['completed'] ?>)</span>
+            </a> |
+        </li>
+        <li>
+            <a href="admin.php?page=dokan-withdraw&amp;status=cancelled" <?php if ( $status == 'cancelled' ) echo 'class="current"'; ?>>
+                <?php _e( 'Cancelled', 'dokan' ); ?> <span class="count">(<?php echo $counts['cancelled'] ?>)</span>
+            </a>
+        </li>
     </ul>
 
     <?php
-    $dokan_admin_withdraw = Dokan_Template_Withdraw::init();
+
     $dokan_admin_withdraw->admin_withdraw_list( $status );
     ?>
 </div>
