5e2045bfc36ba315a293e24425a4edf685894d2b
Tareq Hasan <tareq1988@gmail.com>
2014-01-30 18:39:59 +0600

    new withdraw and approve mail

--- classes/email.php
+++ classes/email.php
@@ -5,10 +5,6 @@
  */
 class Dokan_Email {
 
-    function __construct() {
-
-    }
-
     public static function init() {
         static $instance = false;
 
@@ -29,7 +25,6 @@ class Dokan_Email {
         return wp_specialchars_decode( esc_html( get_option( 'woocommerce_email_from_name' ) ), ENT_QUOTES );
     }
 
-
     /**
      * Get from email address.
      *
@@ -40,8 +35,19 @@ class Dokan_Email {
         return sanitize_email( get_option( 'woocommerce_email_from_address' ) );
     }
 
+    function admin_email() {
+        return get_option( 'admin_email' );
+    }
+
     function get_user_agent() {
         return substr( $_SERVER['HTTP_USER_AGENT'], 0, 150 );
+    }
+
+    function currency_symbol( $amount ) {
+        $price = sprintf( get_woocommerce_price_format(), get_woocommerce_currency_symbol(), $amount );
+        $price = str_replace( array('&#163;', '&#8364;', '&#36;'), array('£', 'EUR', '$'), $price);
+
+        return $price;
     }
 
     function contact_seller( $seller_email, $from_name, $from_email, $message ) {
@@ -75,6 +81,57 @@ class Dokan_Email {
         $headers = array( "Reply-To: {$from_name}<{$from_email}>" );
 
         $this->send( $seller_email, $subject, $body, $headers );
+    }
+
+    function prepare_withdraw( $body, $user, $amount, $method ) {
+        $find = array(
+            '%username%',
+            '%amount%',
+            '%method%',
+            '%profile_url%',
+            '%withdraw_page%',
+            '%site_name%',
+            '%site_url%'
+        );
+
+        $replace = array(
+            $user->user_login,
+            $this->currency_symbol( $amount ),
+            dokan_withdraw_get_method_title( $method ),
+            admin_url( 'user-edit.php?user_id=' . $user->ID ),
+            admin_url( 'admin.php?page=dokan-withdraw' ),
+            $this->get_from_name(),
+            home_url()
+        );
+
+        $body = str_replace( $find, $replace, $body);
+
+        return $body;
+    }
+
+    function new_withdraw_request( $user, $amount, $method ) {
+        $template = DOKAN_INC_DIR . '/emails/withdraw-new.php';
+        ob_start();
+        include $template;
+        $body = ob_get_clean();
+
+        $subject = sprintf( __( '[%s] New Withdraw Request', 'dokan' ), $this->get_from_name() );
+        $body = $this->prepare_withdraw( $body, $user, $amount, $method );
+
+        $this->send( $this->admin_email(), $subject, $body );
+    }
+
+    function withdraw_request_approve( $user_id, $amount, $method ) {
+        $template = DOKAN_INC_DIR . '/emails/withdraw-approve.php';
+        ob_start();
+        include $template;
+        $body = ob_get_clean();
+
+        $user = get_user_by( 'id', $user_id );
+        $subject = sprintf( __( '[%s] Your Withdraw Request has been approved', 'dokan' ), $this->get_from_name() );
+        $body = $this->prepare_withdraw( $body, $user, $amount, $method );
+
+        $this->send( $this->admin_email(), $subject, $body );
     }
 
     /**
