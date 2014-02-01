0173e8d1e2bfbd4ce7cfc3fb66710743b196ee5e
Tareq Hasan <tareq1988@gmail.com>
2014-02-02 00:51:57 +0600

    page width fix

--- page.php
+++ page.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 
-<div id="primary" class="content-area col-md-8">
+<div id="primary" class="content-area col-md-9">
     <div id="content" class="site-content" role="main">
 
         <?php while (have_posts()) : the_post(); ?>
