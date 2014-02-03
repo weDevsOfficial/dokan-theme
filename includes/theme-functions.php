7a0f3cba56c74c7a09d6d5e8adfd645e01a3c8cc
Tareq Hasan <tareq1988@gmail.com>
2014-02-03 15:39:26 +0600

    seller checking updated

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/order-functions.php';
 require_once __DIR__ . '/withdraw-functions.php';
 
 function dokan_is_user_seller( $user_id ) {
-    if ( !user_can( $user_id, 'edit_shop_orders' ) ) {
+    if ( !user_can( $user_id, 'dokandar' ) ) {
         return false;
     }
 
