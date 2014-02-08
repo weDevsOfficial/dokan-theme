467d12b3b473a44ed68ca01f07dea9d2c2bad277
Tareq Hasan <tareq1988@gmail.com>
2014-02-08 19:10:04 +0600

    counts added on admin dashboard

--- includes/admin/dashboard.php
+++ includes/admin/dashboard.php
@@ -21,9 +21,26 @@ function dokan_admin_dash_widget_news() {
 
 function dokan_admin_dash_metabox_glance() {
     $user_count = count_users();
+    $withdraw_counts = dokan_get_withdraw_count();
     $seller_count = isset( $user_count['avail_roles']['seller'] ) ? $user_count['avail_roles']['seller'] : 0;
+    ?>
 
-    var_dump($seller_count);
+    <ul class="main">
+        <li class="seller-count">
+            <a href="<?php echo admin_url( 'admin.php?page=dokan-sellers' ); ?>"><?php printf( __( '%d Sellers', 'dokan' ), $seller_count ); ?></a>
+        </li>
+        <li class="withdraw-pending">
+            <a href="<?php echo admin_url( 'admin.php?page=dokan-withdraw' ); ?>"><?php printf( __( '%d Pending Withdraw', 'dokan' ), $withdraw_counts['pending'] ); ?></a>
+        </li>
+        <li class="withdraw-completed">
+            <a href="<?php echo admin_url( 'admin.php?page=dokan-withdraw&amp;status=completed' ); ?>"><?php printf( __( '%d Completed Withdraw', 'dokan' ), $withdraw_counts['completed'] ); ?></a>
+        </li>
+        <li class="withdraw-pending">
+            <a href="<?php echo admin_url( 'admin.php?page=dokan-withdraw&amp;status=cancelled' ); ?>"><?php printf( __( '%d Cancelled Withdraw', 'dokan' ), $withdraw_counts['cancelled'] ); ?></a>
+        </li>
+    </ul>
+
+    <?php
 }
 
 ?>
@@ -35,12 +52,16 @@ function dokan_admin_dash_metabox_glance() {
         <div class="post-box-container">
             <div class="meta-box-sortables">
                 <?php dokan_admin_dash_metabox( __( 'At a Glance', 'dokan' ), 'dokan_admin_dash_metabox_glance' ); ?>
+
+                <?php do_action( 'dokan_admin_dashboard_metabox_left' ); ?>
             </div>
         </div> <!-- .post-box-container -->
 
         <div class="post-box-container">
             <div class="meta-box-sortables">
                 <?php dokan_admin_dash_metabox( __( 'Dokan News Updates', 'dokan' ), 'dokan_admin_dash_widget_news'); ?>
+
+                <?php do_action( 'dokan_admin_dashboard_metabox_right' ); ?>
             </div>
         </div> <!-- .post-box-container -->
 
