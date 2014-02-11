a959f10a325b916fe9fc8927215e6a5df1d33fc9
Tareq Hasan <tareq1988@gmail.com>
2014-02-11 12:44:51 +0600

    404 page styling

--- 404.php
+++ 404.php
@@ -21,32 +21,32 @@ get_header();
 
                 <?php get_search_form(); ?>
 
-                <div class="row">
-                    <div class="col-md-5">
-                        <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="widget">
-                            <h2 class="widgettitle"><?php _e( 'Most Used Categories', 'dokan' ); ?></h2>
-                            <ul>
-                                <?php wp_list_categories( array('orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10) ); ?>
-                            </ul>
-                        </div><!-- .widget -->
-                    </div>
-
-                    <div class="col-md-3">
-                        <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-                        <?php
-                        /* translators: %1$s: smilie */
-                        $archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'dokan' ), convert_smilies( ':)' ) ) . '</p>';
-                        the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-                        ?>
-                    </div>
-                </div>
-
             </div><!-- .entry-content -->
         </article><!-- #post-0 .post .error404 .not-found -->
+
+        <div class="row">
+            <div class="col-md-4">
+                <?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+            </div>
+            <div class="col-md-4">
+                <div class="widget">
+                    <h2 class="widgettitle"><?php _e( 'Categories', 'dokan' ); ?></h2>
+                    <ul>
+                        <?php wp_list_categories( array('orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10) ); ?>
+                    </ul>
+                </div><!-- .widget -->
+            </div>
+
+            <div class="col-md-4">
+                <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+
+                <?php
+                /* translators: %1$s: smilie */
+                $archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'dokan' ), convert_smilies( ':)' ) ) . '</p>';
+                the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+                ?>
+            </div>
+        </div>
 
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
