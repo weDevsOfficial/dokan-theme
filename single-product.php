87c8eaed3a70970278ceeb9c4895a485aa811287
Tareq Hasan <tareq1988@gmail.com>
2014-02-17 19:35:29 +0600

    previous wc function replace with wc_get_template_part

--- single-product.php
+++ single-product.php
@@ -26,7 +26,7 @@ get_header();
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php woocommerce_get_template_part( 'content', 'single-product' ); ?>
+			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
 		<?php endwhile; // end of the loop. ?>
 
