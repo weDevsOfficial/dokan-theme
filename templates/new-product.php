be7656e18ca47af25b31b5c5dad94495d319b74d
Tareq Hasan <tareq1988@gmail.com>
2014-03-01 13:54:29 +0600

    dokan_can_post filter added

--- templates/new-product.php
+++ templates/new-product.php
@@ -90,76 +90,88 @@ dokan_frontend_dashboard_scripts();
             </div>
         <?php } ?>
 
-        <?php if ( dokan_is_seller_enabled( get_current_user_id() ) ) { ?>
+        <?php
 
-        <form class="form" method="post">
+        $can_sell = apply_filters( 'dokan_can_post', true );
 
-            <div class="row product-edit-container">
-                <div class="col-md-4">
-                    <div class="dokan-feat-image-upload">
-                        <div class="instruction-inside">
-                            <input type="hidden" name="feat_image_id" class="dokan-feat-image-id" value="0">
-                            <i class="fa fa-cloud-upload"></i>
-                            <a href="#" class="dokan-feat-image-btn btn btn-sm"><?php _e( 'Upload a product cover image', 'dokan' ); ?></a>
+        if ( $can_sell ) {
+
+            if ( dokan_is_seller_enabled( get_current_user_id() ) ) { ?>
+
+            <form class="form" method="post">
+
+                <div class="row product-edit-container">
+                    <div class="col-md-4">
+                        <div class="dokan-feat-image-upload">
+                            <div class="instruction-inside">
+                                <input type="hidden" name="feat_image_id" class="dokan-feat-image-id" value="0">
+                                <i class="fa fa-cloud-upload"></i>
+                                <a href="#" class="dokan-feat-image-btn btn btn-sm"><?php _e( 'Upload a product cover image', 'dokan' ); ?></a>
+                            </div>
+
+                            <div class="image-wrap dokan-hide">
+                                <a class="close dokan-remove-feat-image">&times;</a>
+                                    <img src="" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <input class="form-control" name="post_title" id="post-title" type="text" placeholder="Product name.." value="<?php echo dokan_posted_input( 'post_title' ); ?>">
                         </div>
 
-                        <div class="image-wrap dokan-hide">
-                            <a class="close dokan-remove-feat-image">&times;</a>
-                                <img src="" alt="">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><?php echo get_woocommerce_currency_symbol(); ?></span>
+                                <input class="form-control" name="price" id="product-price" type="text" placeholder="9.99" value="<?php echo dokan_posted_input( 'price' ); ?>">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <textarea name="post_excerpt" id="post-excerpt" rows="5" class="form-control" placeholder="Short description about the product..."><?php echo dokan_posted_textarea( 'post_excerpt' ); ?></textarea>
+                        </div>
+
+                        <div class="form-group">
+                        <?php
+                        wp_dropdown_categories( array(
+                            'show_option_none' => __( '- Select a category -', 'dokan' ),
+                            'hierarchical' => 1,
+                            'hide_empty' => 0,
+                            'name' => 'product_cat',
+                            'id' => 'product_cat',
+                            'taxonomy' => 'product_cat',
+                            'title_li' => '',
+                            'class' => 'product_cat form-control',
+                            'exclude' => '',
+                            'selected' => $product_cat,
+                        ) );
+                        ?>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <input class="form-control" name="post_title" id="post-title" type="text" placeholder="Product name.." value="<?php echo dokan_posted_input( 'post_title' ); ?>">
-                    </div>
 
-                    <div class="form-group">
-                        <div class="input-group">
-                            <span class="input-group-addon"><?php echo get_woocommerce_currency_symbol(); ?></span>
-                            <input class="form-control" name="price" id="product-price" type="text" placeholder="9.99" value="<?php echo dokan_posted_input( 'price' ); ?>">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <textarea name="post_excerpt" id="post-excerpt" rows="5" class="form-control" placeholder="Short description about the product..."><?php echo dokan_posted_textarea( 'post_excerpt' ); ?></textarea>
-                    </div>
-
-                    <div class="form-group">
-                    <?php
-                    wp_dropdown_categories( array(
-                        'show_option_none' => __( '- Select a category -', 'dokan' ),
-                        'hierarchical' => 1,
-                        'hide_empty' => 0,
-                        'name' => 'product_cat',
-                        'id' => 'product_cat',
-                        'taxonomy' => 'product_cat',
-                        'title_li' => '',
-                        'class' => 'product_cat form-control',
-                        'exclude' => '',
-                        'selected' => $product_cat,
-                    ) );
-                    ?>
-                    </div>
+                <!-- <textarea name="post_content" id="" cols="30" rows="10" class="span7" placeholder="Describe your product..."><?php echo dokan_posted_textarea( 'post_content' ); ?></textarea> -->
+                <div class="form-group">
+                    <?php wp_editor( $post_content, 'post_content', array('editor_height' => 50, 'quicktags' => false, 'media_buttons' => false, 'teeny' => true, 'editor_class' => 'post_content') ); ?>
                 </div>
-            </div>
 
-            <!-- <textarea name="post_content" id="" cols="30" rows="10" class="span7" placeholder="Describe your product..."><?php echo dokan_posted_textarea( 'post_content' ); ?></textarea> -->
-            <div class="form-group">
-                <?php wp_editor( $post_content, 'post_content', array('editor_height' => 50, 'quicktags' => false, 'media_buttons' => false, 'teeny' => true, 'editor_class' => 'post_content') ); ?>
-            </div>
+                <?php do_action( 'dokan_new_product_form' ); ?>
 
-            <?php do_action( 'dokan_new_product_form' ); ?>
+                <div class="form-group">
+                    <input type="submit" name="add_product" class="btn btn-primary" value="Add Product"/>
+                </div>
 
-            <div class="form-group">
-                <input type="submit" name="add_product" class="btn btn-primary" value="Add Product"/>
-            </div>
+            </form>
 
-        </form>
+            <?php } else { ?>
+
+                <?php dokan_seller_not_enabled_notice(); ?>
+
+            <?php } ?>
 
         <?php } else { ?>
 
-            <?php dokan_seller_not_enabled_notice(); ?>
+            <?php do_action( 'dokan_can_post_notice' ); ?>
 
         <?php } ?>
     </div><!-- #content .site-content -->
