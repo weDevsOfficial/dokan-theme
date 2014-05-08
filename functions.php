3b81b96475a70e1d3cc4f471a3c4aaaa9046980a
Tareq Hasan <tareq1988@gmail.com>
2014-05-09 01:40:45 +0600

    wp_title fix for homepage

--- functions.php
+++ functions.php
@@ -450,3 +450,39 @@ function dokan_admin_toolbar() {
 
 // Hook into the 'wp_before_admin_bar_render' action
 add_action( 'wp_before_admin_bar_render', 'dokan_admin_toolbar' );
+
+/**
+ * Create a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since Dokan 1.0.4
+ *
+ * @param string $title Default title text for current view.
+ * @param string $sep Optional separator.
+ * @return string The filtered title.
+ */
+function dokan_wp_title( $title, $sep ) {
+    global $paged, $page;
+
+    if ( is_feed() ) {
+        return $title;
+    }
+
+    // Add the site name.
+    $title .= get_bloginfo( 'name' );
+
+    // Add the site description for the home/front page.
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) ) {
+        $title = "$title $sep $site_description";
+    }
+
+    // Add a page number if necessary.
+    if ( $paged >= 2 || $page >= 2 ) {
+        $title = "$title $sep " . sprintf( __( 'Page %s', 'dokan' ), max( $paged, $page ) );
+    }
+
+    return $title;
+}
+
+add_filter( 'wp_title', 'dokan_wp_title', 10, 2 );
--- header.php
+++ header.php
@@ -16,27 +16,7 @@ if ( !function_exists( 'WC' ) ) {
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php
-    /*
-     * Print the <title> tag based on what is being viewed.
-     */
-    global $page, $paged;
-
-    wp_title( '|', true, 'right' );
-
-    // Add the blog name.
-    bloginfo( 'name' );
-
-    // Add the blog description for the home/front page.
-    $site_description = get_bloginfo( 'description', 'display' );
-    if ( $site_description && ( is_home() || is_front_page() ) )
-        echo " | $site_description";
-
-    // Add a page number if necessary:
-    if ( $paged >= 2 || $page >= 2 )
-        echo ' | ' . sprintf( __( 'Page %s', 'dokan' ), max( $paged, $page ) );
-
-    ?></title>
+<title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
