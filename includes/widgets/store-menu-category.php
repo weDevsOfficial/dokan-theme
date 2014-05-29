e947d256e8bc76813dfdd7e6fb0b7b843ebf616f
Tareq Hasan <tareq1988@gmail.com>
2014-05-29 15:59:28 +0600

    removed unnecessary walker codes

--- includes/widgets/store-menu-category.php
+++ includes/widgets/store-menu-category.php
@@ -3,33 +3,10 @@
 /**
  * Category walker for generating doakn store category
  */
-class Dokan_Store_Category_Walker extends Walker {
+class Dokan_Store_Category_Walker extends Dokan_Category_Walker {
 
     function __construct( $seller_id ) {
         $this->store_url = dokan_get_store_url ( $seller_id );
-    }
-
-    var $tree_type = 'category';
-    var $db_fields = array('parent' => 'parent', 'id' => 'term_id'); //TODO: decouple this
-
-    function start_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat( "\t", $depth );
-
-        if ( $depth == 0 ) {
-            $output .= $indent . '<div class="sub-category">' . "\n";
-        } else {
-            $output .= "$indent<ul class='children'>\n";
-        }
-    }
-
-    function end_lvl( &$output, $depth = 0, $args = array() ) {
-        $indent = str_repeat( "\t", $depth );
-
-        if ( $depth == 0 ) {
-            $output .= "$indent</div> <!-- .sub-category -->\n";
-        } else {
-            $output .= "$indent</ul>\n";
-        }
     }
 
     function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
@@ -42,16 +19,6 @@ class Dokan_Store_Category_Walker extends Walker {
             $caret = $args['has_children'] ? ' <span class="caret"></span>' : '';
             $class_name = $args['has_children'] ? ' class="has-children"' : '';
             $output .= $indent . '<li' . $class_name . '><a href="' . $this->store_url . 'section/' . $category->term_id . '">' . $category->name . $caret . '</a>';
-        }
-    }
-
-    function end_el( &$output, $category, $depth = 0, $args = array() ) {
-        $indent = str_repeat( "\t", $depth );
-
-        if ( $depth == 1 ) {
-            $output .= "$indent</div><!-- .sub-block -->\n";
-        } else {
-            $output .= "$indent</li>\n";
         }
     }
 }
