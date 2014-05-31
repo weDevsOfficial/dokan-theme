113873ad8ca1b77c12184930496fc6b5f677033b
Tareq Hasan <tareq1988@gmail.com>
2014-05-31 22:30:50 +0600

    duplicated code removed

--- includes/widgets/store-menu-category.php
+++ includes/widgets/store-menu-category.php
@@ -13,12 +13,14 @@ class Dokan_Store_Category_Walker extends Dokan_Category_Walker {
         extract( $args );
         $indent = str_repeat( "\t", $depth );
 
+        $url = $this->store_url . 'section/' . $category->term_id;
+
         if ( $depth == 1 ) {
-            $output .= $indent . '<div class="sub-block">' . "\n\t" .'<h3><a href="' . $this->store_url . 'section/' . $category->term_id . '">' . $category->name . '</a></h3>' . "\n";
+            $output .= $indent . '<div class="sub-block">' . "\n\t" .'<h3><a href="' . $url . '">' . $category->name . '</a></h3>' . "\n";
         } else {
             $caret = $args['has_children'] ? ' <span class="caret"></span>' : '';
             $class_name = $args['has_children'] ? ' class="has-children"' : '';
-            $output .= $indent . '<li' . $class_name . '><a href="' . $this->store_url . 'section/' . $category->term_id . '">' . $category->name . $caret . '</a>';
+            $output .= $indent . '<li' . $class_name . '><a href="' . $url . '">' . $category->name . $caret . '</a>';
         }
     }
 }
