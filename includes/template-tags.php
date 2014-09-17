d64c476baace2b79ed13dadcadfd22f8a9eab7f0
Tareq Hasan <tareq1988@gmail.com>
2014-09-17 19:37:03 +0600

    functions removed

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -208,156 +208,6 @@ function dokan_page_navi( $before = '', $after = '', $wp_query ) {
 
 endif;
 
-function dokan_product_dashboard_errors() {
-    $type = isset( $_GET['msg'] ) ? $_GET['msg'] : '';
-
-    switch ($type) {
-        case 'product_deleted':
-            ?>
-            <div class="alert alert-success">
-                <?php echo __( 'Product has been deleted successfully!', 'dokan' ); ?>
-            </div>
-            <?php
-            break;
-
-        case 'error':
-            ?>
-            <div class="alert alert-danger">
-                <?php echo __( 'Something went wrong!', 'dokan' ); ?>
-            </div>
-            <?php
-            break;
-    }
-}
-
-function dokan_product_listing_status_filter() {
-    $permalink = get_permalink();
-    $status_class = isset( $_GET['post_status'] ) ? $_GET['post_status'] : 'all';
-    $post_counts = dokan_count_posts( 'product', get_current_user_id() );
-    ?>
-    <ul class="list-inline col-md-9 subsubsub">
-        <li<?php echo $status_class == 'all' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo $permalink; ?>"><?php printf( __( 'All (%d)', 'dokan' ), $post_counts->total ); ?></a>
-        </li>
-        <li<?php echo $status_class == 'publish' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'post_status' => 'publish' ), $permalink ); ?>"><?php printf( __( 'Online (%d)', 'dokan' ), $post_counts->publish ); ?></a>
-        </li>
-        <li<?php echo $status_class == 'pending' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'post_status' => 'pending' ), $permalink ); ?>"><?php printf( __( 'Pending Review (%d)', 'dokan' ), $post_counts->pending ); ?></a>
-        </li>
-        <li<?php echo $status_class == 'draft' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'post_status' => 'draft' ), $permalink ); ?>"><?php printf( __( 'Draft (%d)', 'dokan' ), $post_counts->draft ); ?></a>
-        </li>
-        <?php do_action( 'dokan_product_listing_status_filter', $status_class, $post_counts ); ?>
-    </ul> <!-- .post-statuses-filter -->
-    <?php
-}
-
-function dokan_order_listing_status_filter() {
-    $orders_url = get_permalink();
-    $status_class = isset( $_GET['order_status'] ) ? $_GET['order_status'] : 'all';
-    $orders_counts = dokan_count_orders( get_current_user_id() );
-    ?>
-
-    <ul class="list-inline order-statuses-filter">
-        <li<?php echo $status_class == 'all' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo $orders_url; ?>">
-                <?php printf( __( 'All (%d)', 'dokan' ), $orders_counts->total ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'completed' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'completed' ), $orders_url ); ?>">
-                <?php printf( __( 'Completed (%d)', 'dokan' ), $orders_counts->completed ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'processing' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'processing' ), $orders_url ); ?>">
-                <?php printf( __( 'Processing (%d)', 'dokan' ), $orders_counts->processing ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'on-hold' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'on-hold' ), $orders_url ); ?>">
-                <?php printf( __( 'On-hold (%d)', 'dokan' ), $orders_counts->{'on-hold'} ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'pending' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'pending' ), $orders_url ); ?>">
-                <?php printf( __( 'Pending (%d)', 'dokan' ), $orders_counts->pending ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'canceled' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'cancelled' ), $orders_url ); ?>">
-                <?php printf( __( 'Cancelled (%d)', 'dokan' ), $orders_counts->cancelled ); ?></span>
-            </a>
-        </li>
-        <li<?php echo $status_class == 'refunded' ? ' class="active"' : ''; ?>>
-            <a href="<?php echo add_query_arg( array( 'order_status' => 'refunded' ), $orders_url ); ?>">
-                <?php printf( __( 'Refunded (%d)', 'dokan' ), $orders_counts->refunded ); ?></span>
-            </a>
-        </li>
-    </ul>
-    <?php
-}
-
-function dokan_get_dashboard_nav() {
-    $urls = array(
-        'dashboard' => array(
-            'title' => __( 'Dashboard', 'dokan'),
-            'icon' => '<i class="icon-dashboard"></i>',
-            'url' => dokan_get_page_url( 'dashboard' )
-        ),
-        'product' => array(
-            'title' => __( 'Products', 'dokan'),
-            'icon' => '<i class="icon-briefcase"></i>',
-            'url' => dokan_get_page_url( 'products' )
-        ),
-        'order' => array(
-            'title' => __( 'Orders', 'dokan'),
-            'icon' => '<i class="icon-basket"></i>',
-            'url' => dokan_get_page_url( 'orders' )
-        ),
-        'coupon' => array(
-            'title' => __( 'Coupons', 'dokan'),
-            'icon' => '<i class="icon-gift"></i>',
-            'url' => dokan_get_page_url( 'coupons' )
-        ),
-        'report' => array(
-            'title' => __( 'Reports', 'dokan'),
-            'icon' => '<i class="icon-stats"></i>',
-            'url' => dokan_get_page_url( 'reports' )
-        ),
-        'reviews' => array(
-            'title' => __( 'Reviews', 'dokan'),
-            'icon' => '<i class="icon-bubbles"></i>',
-            'url' => dokan_get_page_url( 'reviews' )
-        ),
-        'withdraw' => array(
-            'title' => __( 'Withdraw', 'dokan'),
-            'icon' => '<i class="icon-upload"></i>',
-            'url' => dokan_get_page_url( 'withdraw' )
-        ),
-        'settings' => array(
-            'title' => __( 'Settings', 'dokan'),
-            'icon' => '<i class="icon-cog"></i>',
-            'url' => dokan_get_page_url( 'settings' )
-        ),
-    );
-
-    return apply_filters( 'dokan_get_dashboard_nav', $urls );
-}
-
-function dokan_dashboard_nav( $active_menu ) {
-    $urls = dokan_get_dashboard_nav();
-    $menu = '<ul class="dokan-dashboard-menu">';
-
-    foreach ($urls as $key => $item) {
-        $class = ( $active_menu == $key ) ? ' class="active"' : '';
-        $menu .= sprintf( '<li%s><a href="%s">%s %s</a></li>', $class, $item['url'], $item['icon'], $item['title'] );
-    }
-    $menu .= '</ul>';
-
-    return $menu;
-}
 
 if ( ! function_exists( 'dokan_category_widget' ) ) :
 
@@ -381,133 +231,6 @@ function dokan_category_widget() {
 
 endif;
 
-if ( ! function_exists( 'dokan_store_category_menu' ) ) :
-
-/**
- * Store category menu for a store
- *
- * @param  int $seller_id
- * @return void
- */
-function dokan_store_category_menu( $seller_id ) { ?>
-    <aside class="widget dokan-category-menu">
-        <h3 class="widget-title"><?php _e( 'Store Product Category', 'dokan' ); ?></h3>
-        <div id="cat-drop-stack">
-            <?php
-            global $wpdb;
-
-            $categories = get_transient( 'dokan-store-category-'.$seller_id );
-
-            if ( false === $categories ) {
-                $sql = "SELECT t.term_id,t.name, tt.parent FROM $wpdb->terms as t
-                        LEFT JOIN $wpdb->term_taxonomy as tt on t.term_id = tt.term_id
-                        LEFT JOIN $wpdb->term_relationships AS tr on tt.term_taxonomy_id = tr.term_taxonomy_id
-                        LEFT JOIN $wpdb->posts AS p on tr.object_id = p.ID
-                        WHERE tt.taxonomy = 'product_cat'
-                        AND p.post_type = 'product'
-                        AND p.post_status = 'publish'
-                        AND p.post_author = $seller_id GROUP BY t.term_id";
-
-                $categories = $wpdb->get_results( $sql );
-                set_transient( 'dokan-store-category-'.$seller_id , $categories );
-            }
-
-            $args = array(
-                'taxonomy' => 'product_cat',
-                'selected_cats' => ''
-            );
-
-            $walker = new Dokan_Store_Category_Walker( $seller_id );
-            echo "<ul>";
-            echo call_user_func_array( array(&$walker, 'walk'), array($categories, 0, array()) );
-            echo "</ul>";
-            ?>
-        </div>
-    </aside>
-<?php
-}
-
-endif;
-
-
-function dokan_store_category_delete_transient( $post_id ) {
-
-    $post_tmp = get_post($post_id);
-    $seller_id = $post_tmp->post_author;
-    //delete store category transient
-    delete_transient( 'dokan-store-category-'.$seller_id );
-}
-
-add_action( 'deleted_post', 'dokan_store_category_delete_transient' );
-add_action( 'save_post', 'dokan_store_category_delete_transient' );
-
-
-
-function dokan_seller_reg_form_fields() {
-    $role = isset( $_POST['role'] ) ? $_POST['role'] : 'customer';
-    $role_style = ( $role == 'customer' ) ? ' style="display:none"' : '';
-    ?>
-    <div class="show_if_seller"<?php echo $role_style; ?>>
-
-        <div class="split-row form-row-wide">
-            <p class="form-row form-group">
-                <label for="first-name"><?php _e( 'First Name', 'dokan' ); ?> <span class="required">*</span></label>
-                <input type="text" class="input-text form-control" name="fname" id="first-name" value="<?php if ( ! empty( $_POST['fname'] ) ) echo esc_attr($_POST['fname']); ?>" required="required" />
-            </p>
-
-            <p class="form-row form-group">
-                <label for="last-name"><?php _e( 'Last Name', 'dokan' ); ?> <span class="required">*</span></label>
-                <input type="text" class="input-text form-control" name="lname" id="last-name" value="<?php if ( ! empty( $_POST['lname'] ) ) echo esc_attr($_POST['lname']); ?>" required="required" />
-            </p>
-        </div>
-
-        <p class="form-row form-group form-row-wide">
-            <label for="company-name"><?php _e( 'Shop Name', 'dokan' ); ?> <span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="shopname" id="company-name" value="<?php if ( ! empty( $_POST['shopname'] ) ) echo esc_attr($_POST['shopname']); ?>" required="required" />
-        </p>
-
-        <p class="form-row form-group form-row-wide">
-            <label for="seller-url" class="pull-left"><?php _e( 'Shop URL', 'dokan' ); ?> <span class="required">*</span></label>
-            <strong id="url-alart-mgs" class="pull-right"></strong>
-            <input type="text" class="input-text form-control" name="shopurl" id="seller-url" value="<?php if ( ! empty( $_POST['shopurl'] ) ) echo esc_attr($_POST['shopurl']); ?>" required="required" />
-            <small><?php echo home_url(); ?>/store/<strong id="url-alart"></strong></small>
-        </p>
-
-        <p class="form-row form-group form-row-wide">
-            <label for="seller-address"><?php _e( 'Address', 'dokan' ); ?><span class="required">*</span></label>
-            <textarea type="text" id="seller-address" name="address" class="form-control input" required="required"><?php if ( ! empty( $_POST['address'] ) ) echo esc_textarea($_POST['address']); ?></textarea>
-        </p>
-
-        <p class="form-row form-group form-row-wide">
-            <label for="shop-phone"><?php _e( 'Phone', 'dokan' ); ?><span class="required">*</span></label>
-            <input type="text" class="input-text form-control" name="phone" id="shop-phone" value="<?php if ( ! empty( $_POST['phone'] ) ) echo esc_attr($_POST['phone']); ?>" required="required" />
-        </p>
-    </div>
-
-    <p class="form-row form-group user-role">
-        <label class="radio">
-            <input type="radio" name="role" value="customer"<?php checked( $role, 'customer' ); ?>>
-            <?php _e( 'I am a customer', 'dokan' ); ?>
-        </label>
-
-        <label class="radio">
-            <input type="radio" name="role" value="seller"<?php checked( $role, 'seller' ); ?>>
-            <?php _e( 'I am a seller', 'dokan' ); ?>
-        </label>
-    </p>
-    <?php
-}
-
-add_action( 'register_form', 'dokan_seller_reg_form_fields' );
-
-function dokan_seller_not_enabled_notice() {
-    ?>
-        <div class="alert alert-warning">
-            <strong><?php _e( 'Error!', 'dokan' ); ?></strong>
-            <?php _e( 'Your account is not enabled for selling, please contact the admin', 'dokan' ); ?>
-        </div>
-    <?php
-}
 
 if ( !function_exists( 'dokan_header_user_menu' ) ) :
 
@@ -557,8 +280,8 @@ function dokan_header_user_menu() {
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo esc_html( $current_user->display_name ); ?> <b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="<?php echo dokan_get_page_url( 'my_orders' ); ?>"><?php _e( 'My Orders', 'dokan' ); ?></a></li>
-                    <li><a href="<?php echo dokan_get_page_url( 'myaccount', 'dokan' ); ?>"><?php _e( 'My Account', 'dokan' ); ?></a></li>
+                    <li><a href="<?php echo get_permalink( wc_get_page_id( 'my_orders' ) ); ?>"><?php _e( 'My Orders', 'dokan' ); ?></a></li>
+                    <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'My Account', 'dokan' ); ?></a></li>
                     <li><a href="<?php echo wc_customer_edit_account_url(); ?>"><?php _e( 'Edit Account', 'dokan' ); ?></a></li>
                     <li class="divider"></li>
                     <li><a href="<?php echo wc_get_endpoint_url( 'edit-address', 'billing', get_permalink( wc_get_page_id( 'myaccount' ) ) ); ?>"><?php _e( 'Billing Address', 'dokan' ); ?></a></li>
@@ -569,26 +292,11 @@ function dokan_header_user_menu() {
             <li><?php wp_loginout( home_url() ); ?></li>
 
         <?php } else { ?>
-            <li><a href="<?php echo dokan_get_page_url( 'myaccount', 'woocommerce' ); ?>"><?php _e( 'Log in', 'dokan' ); ?></a></li>
-            <li><a href="<?php echo dokan_get_page_url( 'myaccount', 'woocommerce' ); ?>"><?php _e( 'Sign Up', 'dokan' ); ?></a></li>
+            <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Log in', 'dokan' ); ?></a></li>
+            <li><a href="<?php echo get_permalink( wc_get_page_id( 'myaccount' ) ); ?>"><?php _e( 'Sign Up', 'dokan' ); ?></a></li>
         <?php } ?>
     </ul>
     <?php
 }
 
 endif;
-
-function dokan_account_migration_button() {
-    $user = wp_get_current_user();
-
-    if ( dokan_is_user_customer( $user->ID ) ) {
-        ?>
-        <p>&nbsp;</p>
-        <p>
-            <a href="<?php echo dokan_get_page_url( 'myaccount', 'woocommerce' ); ?>account-migration/seller/" class="button button-primary"><?php _e( 'Become a Seller', 'dokan' ); ?></a>
-        </p>
-        <?php
-    }
-}
-
-add_action( 'woocommerce_after_my_account', 'dokan_account_migration_button' );
