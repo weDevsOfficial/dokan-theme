a7ba534d8770da6bb7d74105588a501ce0ad11e7
Tareq Hasan <tareq1988@gmail.com>
2015-06-20 19:48:05 +0600

    grunt release tasks added

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,70 @@ module.exports = function(grunt) {
                     }
                 }
             }
-        }
+        },
+
+        // Clean up build directory
+        clean: {
+            main: ['build/']
+        },
+
+        // Copy the plugin into the build directory
+        copy: {
+            main: {
+                src: [
+                    '**',
+                    '!node_modules/**',
+                    '!build/**',
+                    '!bin/**',
+                    '!.git/**',
+                    '!Gruntfile.js',
+                    '!package.json',
+                    '!debug.log',
+                    '!phpunit.xml',
+                    '!.gitignore',
+                    '!.gitmodules',
+                    '!npm-debug.log',
+                    '!assets/less/**',
+                    '!tests/**',
+                    '!**/Gruntfile.js',
+                    '!**/package.json',
+                    '!**/README.md',
+                    '!**/export.sh',
+                    '!**/*~'
+                ],
+                dest: 'build/'
+            }
+        },
+
+        //Compress build directory into <name>.zip and <name>-<version>.zip
+        compress: {
+            main: {
+                options: {
+                    mode: 'zip',
+                    archive: './build/dokan-theme.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: 'dokan'
+            }
+        },
     });
 
     // Load NPM tasks to be used here
     grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-copy' );
+    grunt.loadNpmTasks( 'grunt-contrib-compress' );
+
+
+    grunt.registerTask('release', [
+        'makepot',
+    ]);
+
+    grunt.registerTask( 'zip', [
+        'clean',
+        'copy',
+        'compress'
+    ])
 };
