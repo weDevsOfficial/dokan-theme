c0b43b5bfe78673f1389f0e3350484d3bcd84acf
rafsuntaskin <rafsun.aiub@gmail.com>
2016-01-20 12:03:41 +0600

    woocommerce outdated template updated for add-to-cart

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -1,12 +1,20 @@
+
 <?php
 /**
  * Loop Add to Cart
  *
- * @author      WooThemes
- * @package     WooCommerce/Templates
- * @version     2.1.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+ * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+ * as little as possible, but it does happen. When this occurs the version of the template file will.
+ * be bumped and the readme will list any important changes.
+ *
+ * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.5.0
  */
-
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product;
@@ -14,11 +22,13 @@ global $product;
 $icon_class = ($product->product_type == 'variable' ) ? 'fa-bars' : 'fa-shopping-cart';
 
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-    sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="cat btn add_to_cart_button product_type_%s" title="%s">%s</a>',
+    sprintf( '<a href="%s" rel="nofollow" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="cat btn add_to_cart_button product_type_%s" title="%s">%s</a>',
         esc_url( $product->add_to_cart_url() ),
+        esc_attr( isset( $quantity ) ? $quantity : 1 ),
         esc_attr( $product->id ),
         esc_attr( $product->get_sku() ),
         esc_attr( $product->product_type ),
+        esc_attr( isset( $class ) ? $class : 'button' ),
         esc_html( $product->add_to_cart_text() ),
         sprintf( '<i class="fa %s"></i>', $icon_class )
     ),
