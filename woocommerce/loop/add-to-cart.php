91df2f72032e297a9aeb24cc62cd1e145515eca2
rafsuntaskin <rafsun.aiub@gmail.com>
2016-07-31 10:27:43 +0600

    add to cart icon fixed

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -19,15 +19,14 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $product;
 
-$icon_class = ($product->product_type == 'variable' ) ? 'fa-bars' : 'fa-shopping-cart';
+$icon_class = ( $product->product_type == 'simple' ) ? 'fa-shopping-cart' : 'fa-bars';
 
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-    sprintf( '<a href="%s" rel="nofollow" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="cat btn add_to_cart_button product_type_%s" title="%s">%s</a>',
+    sprintf( '<a href="%s" rel="nofollow" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="cat btn add_to_cart_button %s" title="%s">%s</a>',
         esc_url( $product->add_to_cart_url() ),
         esc_attr( isset( $quantity ) ? $quantity : 1 ),
         esc_attr( $product->id ),
         esc_attr( $product->get_sku() ),
-        esc_attr( $product->product_type ),
         esc_attr( isset( $class ) ? $class : 'button' ),
         esc_html( $product->add_to_cart_text() ),
         sprintf( '<i class="fa %s"></i>', $icon_class )
