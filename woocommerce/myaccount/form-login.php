88bf7b8f11af058c8474cb60af8d6f7d9e053f81
Sabbir Ahmed <sabbir.081070@gmail.com>
2016-11-13 23:16:34 +0600

    Update outdated woocommerce template warning

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -2,12 +2,23 @@
 /**
  * Login Form
  *
- * @author      WooThemes
- * @package     WooCommerce/Templates
- * @version     2.6.0
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/form-login.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.6.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
 ?>
 
 <?php wc_print_notices(); ?>
