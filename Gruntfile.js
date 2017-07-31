4497e529d4836ac251f493fd2eff11c4f8d03f1a
Sabbir Ahmed <sabbir.081070@gmail.com>
2017-08-01 00:04:49 +0600

    Version bump, update grunt and sorucemap

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
             // one to one
             core: {
                 options: {
-                    sourceMap: true,
+                    sourceMap: false,
                     sourceMapFilename: 'style.css.map',
                     sourceMapURL: 'style.css.map',
                     sourceMapRootpath: '../../'
